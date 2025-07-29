e5b0199ba4cbd5440c1ef4b0c425d0ba60988fed
Bhuwan Vamsi <112535744+bhuwan-vamsi@users.noreply.github.com>
2025-07-29 22:08:30 +0530

    Add Category Static

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,8 +1,10 @@
-﻿using APIPractice.CustomAcitonFilters;
+﻿using APIPractice.Models.Domain;
+using APIPractice.CustomAcitonFilters;
 using APIPractice.Models.DTO;
 using APIPractice.Services.IService;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
@@ -48,6 +50,59 @@ namespace APIPractice.Controllers
                 await customerService.EditProfile(identity, request);
                 return Ok("Successfully Updated");
             }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            try
+            {
+                var categories = new List<CustomerCategoryDto>
+                {
+                    new CustomerCategoryDto
+                    {
+                        Id = Guid.Parse("75BD72C3-9150-4EC7-B2A9-A47E38744CAC"),
+                        Name = "Baby Care",
+                        ImageUrl = ""
+                    },
+                    new CustomerCategoryDto
+                    {
+                        Id = Guid.Parse("0100A0A6-8237-47E7-B158-7A40B4808E85"),
+                        Name = "Bakery, Cakes & Dairy",
+                        ImageUrl = ""
+                    },
+                    new CustomerCategoryDto
+                    {
+                        Id = Guid.Parse("D20B902C-05DF-4F4E-8647-639DE3319D5B"),
+                        Name = "Beverages",
+                        ImageUrl = ""
+                    },
+                    new CustomerCategoryDto
+                    {
+                        Id = Guid.Parse("5394E988-0A56-41C5-BD6A-3A0B9F2F3177"),
+                        Name = "Eggs, Meat & Fish",
+                        ImageUrl = ""
+                    },
+                    new CustomerCategoryDto
+                    {
+                        Id = Guid.Parse("B1F5A121-D4AD-48F2-8736-30BB6080B2CC"),
+                        Name = "Fruits & Vegetables",
+                        ImageUrl = ""
+                    },
+                    new CustomerCategoryDto
+                    {
+                        Id = Guid.Parse("5633EF72-1953-49B0-B587-95F8A2FC1684"),
+                        Name = "Snacks & Branded Foods",
+                        ImageUrl = ""
+                    }
+                };
+
+                return Ok(categories);
+            }
+            catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
