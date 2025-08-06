b9f408d99fe9abafc464e66ce6c83f42ae71a2ee
drr8xyf <drr8xyf@WS671515>
2025-08-06 06:16:00 +0200

    change

--- Models/DTO/CreateProductDto.cs
+++ Models/DTO/CreateProductDto.cs
@@ -5,16 +5,22 @@ namespace APIPractice.Models.DTO
     public class CreateProductDto
     {
         [Required]
+        [StringLength(100, MinimumLength = 3)]
         public required string Name { get; set; }
         [Required]
+        [Range(1, double.MaxValue)]
         public decimal CostPrice { get; set; }
         [Required]
+        [Range(1, double.MaxValue)]
         public decimal Price { get; set; }
         [Required]
+        [StringLength(15, MinimumLength = 2)]
         public required string Units { get; set; }
         [Required]
+        [Range(1, int.MaxValue)]
         public int Quantity { get; set; }
         [Required]
+        [Range(1, int.MaxValue)]
         public int Threshold { get; set; }
         public string? ImageUrl { get; set; }
         [Required]
--- Models/DTO/UpdateProductDto.cs
+++ Models/DTO/UpdateProductDto.cs
@@ -6,15 +6,21 @@ namespace APIPractice.Models.DTO
     public class UpdateProductDto
     {
         [Required]
+        [StringLength(100, MinimumLength = 3)]
         public required string Name { get; set; }
         [Required]
+        [Range(1, double.MaxValue)]
         public decimal Price { get; set; }
+        [Range(1, double.MaxValue)]
         public decimal CostPrice { get; set; }
         [Required]
+        [StringLength(15, MinimumLength =2)]
         public required string Units { get; set; }
         [Required]
+        [Range(1, int.MaxValue)]
         public int Quantity { get; set; }
         [Required]
+        [Range(1, int.MaxValue)]
         public int Threshold { get; set; }
         public required string ProductStatus { get; set; }
         public string? ImageUrl { get; set; }
